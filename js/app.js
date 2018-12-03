bcd4bc08e0d1e123b6ded3c6cc5f63514ec0162c
Ben Tyler <benandrewtyler@gmail.com>
2018-12-03 11:00:38 +0000

    event listener

--- js/app.js
+++ js/app.js
@@ -26,9 +26,22 @@ function shuffle(array) {
 }
 
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
+/* set up the event listener for a card. If a card is clicked */
+
+const cards = document.querySelectorAll('.card');
+console.log(cards);
+
+for (card of cards) {
+    card.addEventListener('click', () => {
+        console.log("this is a card click");
+    });
+}
+
+
+
+
+
+ /*  - display the card's symbol (put this functionality in another function that you call from this one)
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
  *  - if the list already has another card, check to see if the two cards match
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
